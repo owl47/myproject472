979333402488b39adfa05791602b9f7883b63f19
ahmed alsanhoty <ahmedalsanhoty47@gmail.com>
2022-02-21 14:39:25 +0100

    Initial Push

---
+++ myproject472/lib.py
@@ -0,0 +1,8 @@
+def summon_a_goose():
+    print ('''goose has been summoned   (・ε・),
+            goose is shy (>ε<),
+            goose wishes you don't take it personally   (づ￣ ³￣)づ,
+            but has to say goodbye.   ╰( ⁰ ਊ ⁰ )━☆ﾟ. *･｡ﾟ ''')
+
+def try_me(summon_a_goose):
+    return summon_a_goose()
